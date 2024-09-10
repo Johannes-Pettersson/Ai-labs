592c5bb8db022e3d57dc664c170a603be0d5a6c0
Johannes Pettersson <pejo22ev@student.ju.se>
2024-09-10 13:46:35 +0200

    First A' draft

--- lab1/A'.hs
+++ lab1/A'.hs
@@ -1,39 +1,48 @@
 import Data.List
 
 -- Generic part
-type StatePair = (State,State)
-
+type StateTriple = [State,State,Value]
+type Value = (Int, Int)
+--           (f,   g  )
 run :: [State]
-run = gps [(startState,startParent)] []
+run = A' [[startState, startParent, (calculateHval startState, 0)]] []
 
-gps :: [StatePair]->[StatePair]->[State]
-gps [] _ = error "No solution"
-gps (s:restOfOpen) closed 
-    |   isGoal (fst s) = reverse(retrieveSolution (fst s) (s:closed))  
-    |   otherwise = gps newOpen (s:closed) 
-            where newOpen = restOfOpen ++ zip newStates (repeat (fst s))
+A' :: [StateTriple]->[StateTriple]->[State]
+A' [] _ = error "No solution"
+A' (s:restOfOpen) closed 
+    |   isGoal (fst s) = reverse(retrieveSolution (fst s) (s:closed))
+    |   otherwise = A' newOpen (s:closed)
+            where newOpen = sortByFvals (restOfOpen ++ [[newState, fst s, calculateFval newState] | newState <- newStates])
                   newStates = filter (`notElem` map fst (restOfOpen ++ closed)) (makeMoves (fst s))
              
 
-retrieveSolution :: State -> [StatePair] -> [State]
+retrieveSolution :: State -> [StateTriple] -> [State]
 retrieveSolution s closed = 
     let parent = findParent s closed 
     in if parent == startParent then [s] else s: retrieveSolution parent closed
             where findParent y xs = snd . head $ filter ((== y) . fst) xs
-           
+
+calculateHval :: State -> Int
+--TODO
+
+calculateFval :: StateTriple -> Int
+--TODO
+
+sortByFvals :: [StateTriple] -> [StateTriple]
+--TODO
 
 
 -- Problem specific
-type State = (Int,Int)
+type State = [Int]
 
 startState :: State
-startState = (0,0)
+startState = [0..8]
 
 startParent :: State
-startParent = (-1,-1)
+startParent = []
 
 isGoal :: State -> Bool
-isGoal (x,_) = x==2
+isGoal state = state == [1,2,3,4,5,6,7,8,0]
 
 makeMoves :: State -> [State]
 makeMoves state = filter (/= state) (map ($ state) operators)
@@ -42,26 +51,7 @@ operators :: [State->State]
 operators = [fillSmall, fillLarge, emptySmall, emptyLarge, smallInLargeTillFull,smallInLargeTillEmpty,largeInSmallTillFull,largeInSmallTillEmpty]
 
 fillSmall :: State->State
-fillSmall (x,y) = if y < 3 then (x,3) else (x,y) 
+fillSmall (x,y) = if y < 3 then (x,3) else (x,y)
 
-fillLarge :: State->State
-fillLarge (x,y) = if x < 4 then (4,y) else (x,y)
-
-emptySmall :: State->State
-emptySmall (x,y) = if y > 0 then (x,0) else (x,y)
-
-emptyLarge :: State->State
-emptyLarge (x,y) = if x > 0 then (0,y) else (x,y)
-
-smallInLargeTillFull :: State->State
-smallInLargeTillFull (x,y) = if x+y >= 4 && x < 4 && y > 0 then (4,y-(4-x)) else (x,y)
-
-smallInLargeTillEmpty :: State->State
-smallInLargeTillEmpty (x,y) = if x+y < 4 && y > 0 then (x+y,0) else (x,y)
-
-largeInSmallTillFull :: State->State
-largeInSmallTillFull (x,y) = if x+y >= 3 && y < 3 && x > 0 then (x-(3-y),3) else (x,y)
-
-largeInSmallTillEmpty :: State->State
-largeInSmallTillEmpty (x,y) = if x+y < 3 && y > 0 then (0,x+y) else (x,y)
-
+moveUp :: State->State
+moveUp state = 

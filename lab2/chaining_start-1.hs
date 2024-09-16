7ba9d842ab5f86e2e876d0b2e5cdb80426aab8d8
christophereriksson <erch22im@student.ju.se>
2024-09-16 15:11:45 +0200

    Task 1 finish?

--- lab2/chaining_start-1.hs
+++ lab2/chaining_start-1.hs
@@ -17,11 +17,15 @@ rules = [(["a","b"],["c"]), (["c"],["d"]), (["g"],["h"]),(["a","d"],["f","g"]),(
 runForward :: WM
 runForward = forwardChaining (onePass startWM rules) startWM rules
 
-forwardChaining:: WM -> WM -> RuleSet -> WM
+forwardChaining :: WM -> WM -> RuleSet -> WM
 forwardChaining  newWM oldWM rules = if newWM == oldWM then oldWM else forwardChaining (onePass newWM rules) newWM rules
 
---onePass:: WM -> RuleSet -> WM
---TBD
+onePass :: WM -> RuleSet -> WM
+onePass wm [] = wm  -- if empty, return wm
+onePass wm ((premise, conclusion) :rest) -- split current rule with rest of rules
+  | subSet premise wm = onePass (wm ++ [fact | fact <- conclusion, fact `notElem` wm]) rest -- check if premise is subset of current wm
+  | otherwise = onePass wm rest -- else continue 
+
 
 --backwardChaining :: RuleSet -> Fact -> WM -> Bool
 --TBD

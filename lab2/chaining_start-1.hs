abbb8b11b59bbf4913cf44fdb4764d6fcea8cc0d
christophereriksson <erch22im@student.ju.se>
2024-09-16 15:53:05 +0200

    finish with task 2?

--- lab2/chaining_start-1.hs
+++ lab2/chaining_start-1.hs
@@ -27,5 +27,10 @@ onePass wm ((premise, conclusion) :rest) -- split current rule with rest of rule
   | otherwise = onePass wm rest -- else continue 
 
 
---backwardChaining :: RuleSet -> Fact -> WM -> Bool
---TBD
+backwardChaining :: RuleSet -> Fact -> WM -> Bool
+backwardChaining _ goal wm 
+    | goal `elem` wm = True -- if object is known, return true
+backwardChaining [] _ _ = False -- of no more rules, return false
+backwardChaining rules goal wm =
+    or [all (\fact -> backwardChaining rules fact wm) premise -- if premise of any rule is true, goal is true, else false
+     | (premise, conclusion) <- rules, goal `elem` conclusion]

1870e5bcf068e8074c7985af39c0866af92504b7
Johannes Pettersson <pejo22ev@student.ju.se>
2024-09-17 15:33:21 +0200

    hsfkjs

--- lab2/minimax.py
+++ lab2/minimax.py
@@ -1,17 +1,29 @@
 from typing import Union
 
 class Tree():
-    def __init__(self, left_child, right_child) -> None:
+    def init(self, left_child, right_child) -> None:
         self.left_child = left_child
         self.right_child = right_child
 
+    def print(self):
+        print("Tree")
+        self.left_child.print()
+        self.right_child.print()
+
 class Leaf():
-    def __init__(self, value) -> None:
+    def init(self, value) -> None:
         self.value = value
 
     def get_value(self) -> int:
         return self.value
-    
 
-def create_tree(leaf_values) -> Tree:
-    tree = 
+    def print(self):
+        print(f"Leaf: {self.value}")
+
+def create_tree(leaf_values: list) -> Tree:
+    tree = Tree(Leaf(leaf_values.pop()), Leaf(leaf_values.pop()))
+
+    if len(leaf_values) > 0:
+        tree = Tree(tree, create_tree(leaf_values))
+
+    return tree

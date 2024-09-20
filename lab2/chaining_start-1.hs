138c885cfc569b7affe6d23b71be4fcb88cdfc4d
christophereriksson <erch22im@student.ju.se>
2024-09-20 13:25:11 +0200

    Adding "funny" rules and pictures of the runs

--- lab2/chaining_start-1.hs
+++ lab2/chaining_start-1.hs
@@ -8,11 +8,26 @@ type WM = [Fact]
 subSet :: Eq a => [a] -> [a]-> Bool
 subSet a b = null [x | x<-a , x `notElem` b]
 
+--startWM :: WM
+--startWM = ["a","e"]
+
+--rules :: RuleSet
+--rules = [(["a","b"],["c"]), (["c"],["d"]), (["g"],["h"]),(["a","d"],["f","g"]),(["e"],["d"])]
+
 startWM :: WM
-startWM = ["a","e"]
+startWM = ["katt_sover", "solen_sken", "brottslingar_gralar", "groda_hoppar"]
 
 rules :: RuleSet
-rules = [(["a","b"],["c"]), (["c"],["d"]), (["g"],["h"]),(["a","d"],["f","g"]),(["e"],["d"])]
+rules = [(["katt_sover", "hund_skaller"], ["granne_vaknar"])         -- om katt sover och hund skäller, granne vaknar
+    , (["granne_vaknar"], ["kaffe_bryggs"])                          -- om granne vaknar, då bryggs kaffe
+    , (["tvspel_slapps"], ["gamer_spelar_det"])                      -- om tvspel släpps, gamer spelar det
+    , (["brottsligar_gralar", "kaffe_bryggs"], ["fred_på_jorden"])   -- om bråttslingar grälar och kaffe bryggs, då blir det fred på jorden
+    , (["solen_sken"], ["taco_tillagas"])                            -- om solen skiner, tillagas tacos
+    , (["taco_tillagas", "hund_skaller"], ["hund_far_taco"])         -- om tacos tillagas och hund skäller, då får hunden tacos
+    , (["groda_hoppar", "taco_tillagas"], ["fiesta_borjar"])         -- om grodor hoppar och tacos tillagas, då börjar fiesta
+    , (["fiesta_borjar"], ["brottslingar_dansar"])                   -- om fiesta börjar, då dansar bråttslingarna
+    , (["fred_på_jorden", "gamer_spelar_det"], ["universum_balans"]) -- om fred på jorden och gamer spelar det, då är universum i balans
+    , (["universum_balans"], ["katt_vaknar"])]                       -- om universum är i balans, då vaknar katten
 
 runForward :: WM
 runForward = forwardChaining (onePass startWM rules) startWM rules
